d33a64fd39a513f17d4707322619ce48b2b676bd
cchaeyoung <kimchaeyoung05@naver.com>
2023-02-07 15:38:08 +0900

    fin

--- src/store/list/listSlice.js
+++ src/store/list/listSlice.js
@@ -16,27 +16,27 @@ export const getList = createAsyncThunk(
 
 export const addList = createAsyncThunk(
   "ADD_LIST",
-  async(newList)=>{
-    try{
-        const res= await axios.post(`https://my-json-server.typicode.com/cchaeyoung/redux-2/list/${id}`, newList);
-        return res.data;
-    }catch(err){
-        console.log(err)
+    async(newList)=>{
+        try{
+            const res= await axios.post(`https://my-json-server.typicode.com/cchaeyoung/redux-2/list`, newList);
+            return res.data;
+        }catch(err){
+            console.log(err)
+        }
     }
-  }
 )
 
 
 export const deleteList = createAsyncThunk(
     "DELETE_LIST",
-    async(id)=>{
-        try{
-            const res = await axios.delete(`https://my-json-server.typicode.com/cchaeyoung/redux-2/list/${id}`);
-            return id;
-        }catch(err){
-            console.log(err)
+        async(id)=>{
+            try{
+                const res = await axios.delete(`https://my-json-server.typicode.com/cchaeyoung/redux-2/list/${id}`);
+                return id;
+            }catch(err){
+                console.log(err)
+            }
         }
-    }
 )    
 export const updateList = createAsyncThunk(
     "UPDATE_LIST",
@@ -76,8 +76,8 @@ export const listSlice = createSlice({
     })
     builder.addCase(updateList.fulfilled, (state, action) => {
         state.message = "리스트가 수정되었습니다.";
-        const num =  state.data.findIndex(item=>item.id === action.payload)
-        state.datasplice(num,1,action.payload);
+        const num =  state.data.findIndex(item=>item.id === action.payload.id)
+        state.data.splice(num,1,action.payload);
     })
   },
 })
