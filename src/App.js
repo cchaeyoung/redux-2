5380218ac0b8901d9860915ffe2c9d706cdce4a4
cchaeyoung <kimchaeyoung05@naver.com>
2023-02-07 14:34:21 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import ListItem from './components/ListItem';
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect ,useState} from "react";
+import {getList,addList} from './store/list/listSlice'
+
 
 function App() {
+  const dispatch = useDispatch();
+  const [inputValue, setInputValue] = useState("");
+  const listdata= useSelector((state)=>state.list);
+  useEffect(()=>{
+    dispatch(getList());
+  },[])
+  const onCreate = (e) => {
+    e.preventDefault();
+    if(inputValue){
+      const newList = {content : inputValue }
+      dispatch(addList(newList))
+      setInputValue('');
+    }else{
+      alert('새로운 목록의 내용을 입력하세요')
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onCreate}>
+          <h1>{listdata.message}</h1>
+          <div className='List'>
+            { listdata.data.map(ele=>(
+              <ListItem key={ele.id} content={ele.content} id={ele.id} />
+            ))}
+            
+          </div>
+          <input 
+            type="text"
+            value={inputValue}
+            onChange={(e)=>setInputValue(e.target.value)}
+          />
+          <button type="submit">목록추가</button>
+      </form>
     </div>
   );
 }
---
+++ src/store/list/listSlice.js
@@ -0,0 +1,88 @@
+import axios from "axios";
+import { createSlice , createAsyncThunk} from '@reduxjs/toolkit'
+// createAsyncThunk : Thunk 비동기 작업
+
+export const getList = createAsyncThunk(
+    "GET_LIST",
+    async()=>{
+        try{
+            const res= await axios.get("http://localhost:5000/list");
+            return res.data;
+        }catch(err){
+            console.log(err)
+        }
+    }
+)
+
+export const addList = createAsyncThunk(
+  "ADD_LIST",
+  async(newList)=>{
+    try{
+        const res= await axios.post(`https://my-json-server.typicode.com/cchaeyoung/redux-2/list/${id}`, newList);
+        return res.data;
+    }catch(err){
+        console.log(err)
+    }
+  }
+)
+
+
+export const deleteList = createAsyncThunk(
+    "DELETE_LIST",
+    async(id)=>{
+        try{
+            const res = await axios.delete(`https://my-json-server.typicode.com/cchaeyoung/redux-2/list/${id}`);
+            return id;
+        }catch(err){
+            console.log(err)
+        }
+    }
+)    
+export const updateList = createAsyncThunk(
+    "UPDATE_LIST",
+    async({id, content})=>{
+        try{
+            const res = await axios.put(`https://my-json-server.typicode.com/cchaeyoung/redux-2/list/${id}`, {content: content});
+            return {id,content};
+        }catch(err){
+            console.log(err)
+        }
+    }
+)
+
+const initialState = {
+    data:[],
+    message:'default'
+}
+
+export const listSlice = createSlice({
+  name: 'list',
+  initialState,
+  reducers: {
+    
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getList.fulfilled, (state, action) => {
+          state.message = "리스트업 완료";
+          state.data=action.payload;
+    })
+    builder.addCase(addList.fulfilled, (state, action) => {
+          state.message = "새로운 리스트 추가 완료했습니다.";
+          state.data.push(action.payload);
+    })
+    builder.addCase(deleteList.fulfilled, (state, action) => {
+        state.message = "삭제가 완료되었습니다.";
+        state.data = state.data.filter(item=>item.id !== action.payload)
+    })
+    builder.addCase(updateList.fulfilled, (state, action) => {
+        state.message = "리스트가 수정되었습니다.";
+        const num =  state.data.findIndex(item=>item.id === action.payload)
+        state.datasplice(num,1,action.payload);
+    })
+  },
+})
+
+
+//export const { } = listSlice.actions
+
+export default listSlice.reducer
